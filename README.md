4f59aad55b0565b094dbb49601689f620c33480a
qmahmoudi74 <qmahmoudi74@gmail.com>
2021-05-02 08:49:38 +0430

    add some lib

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Tailwind CSS
 
 ## This is a test project for introducing tailwindcss
+
+### links:
+
+- react tailwind setup: https://tailwindcss.com/docs/guides/create-react-app
+- stylelint: https://dev.to/oliverandrich/vscode-stylelint-tailwind-css-3oag
+
+### vscode extentions:
+
+- tailwindcss intelliSense: https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+- stylelint: https://marketplace.visualstudio.com/items?itemName=stylelint.vscode-stylelint
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import styles from "./styles/App.module.scss";
+
+export default function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles["App-header"]}>
+        <img src={logo} className={styles["App-logo"]} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className={styles["App-link"]}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
