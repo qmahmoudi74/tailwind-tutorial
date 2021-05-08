2f8d7fe03aa162c87fb83dc670b077498003f08e
ehsan mahmodi <e.mahmodi@narvanventures.com>
2021-05-08 14:31:41 +0430

    rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,7 @@ export default function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className={styles["App-link"]}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className={styles["App-link"]} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
